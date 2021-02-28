7fb4942b3066279b4d93ec15ef694e8083a1903f
ValentinSt <stamatevalentin125@gmail.com>
2021-02-28 19:14:57 +0200

    lab_2 added javadoc files

---
+++ Lab_2/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.perosal","l":"Destination"},{"p":"com.perosal","l":"Factory"},{"p":"com.perosal","l":"Main"},{"p":"com.perosal","l":"Problem"},{"p":"com.perosal","l":"Source"},{"p":"com.perosal","l":"SourceType"},{"p":"com.perosal","l":"ToDestination"},{"p":"com.perosal","l":"Warehouse"}];updateSearchResults();
