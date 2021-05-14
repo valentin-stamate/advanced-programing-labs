8b217b50d5f2f2a7c0dee7de64d5ea599d081992
ValentinSt <stamatevalentin125@gmail.com>
2021-05-14 15:13:15 +0300

    added configuration file for deployment

--- lab_11/src/main/java/com/perosal/lab_11/Main.java
+++ lab_11/src/main/java/com/perosal/lab_11/Main.java
@@ -2,8 +2,9 @@ package com.perosal.lab_11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Main {
 
     /* You may find a typo: "persons" instead of "people"... yeah, I missed that */
